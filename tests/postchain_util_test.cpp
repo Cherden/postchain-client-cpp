176b77f8ca0bc11cee64ec96eae4af8538e2c4d6
yevhendev645 <yevhorob@gmail.com>
2021-11-05 14:32:08 +0200

    Isolate tests by macro

---
+++ tests/postchain_util_test.cpp
@@ -0,0 +1,21 @@
+#ifdef POSTCHAIN_TESTS
+#include "../src/postchain_util.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+using namespace chromia::postchain;
+
+TEST(PostchainUtil, LongToByteVectorTest) {}
+
+TEST(PostchainUtil, Sha256Test) {
+    std::string buffer = "password";
+    std::string hashed_buffer =
+        PostchainUtil::Sha256({buffer.begin(), buffer.end()});
+
+    std::string expected_hash =
+        "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+    ASSERT_EQ(expected_hash, hashed_buffer);
+}
+#endif // POSTCHAIN_TESTS

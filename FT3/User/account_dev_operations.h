60e7902ed57f0e16c0735cf8fc941c68deb914c1
yevhendev645 <yevhorob@gmail.com>
2021-11-10 01:04:41 +0200

    account_dev_operations.h

--- FT3/User/account_dev_operations.h
+++ FT3/User/account_dev_operations.h
@@ -1,20 +1,35 @@
-//namespace Chromia.Postchain.Ft3
-//{
-//    public static class AccountDevOperations
-//    {
-//        public static Operation Register(AuthDescriptor authDescriptor)
-//        {
-//            return new Operation("ft3.dev_register_account", new object[] { authDescriptor.ToGTV() });
-//        }
-//
-//        public static Operation FreeOp(string accountID)
-//        {
-//            return new Operation("ft3.dev_free_op", new object[] { accountID });
-//        }
-//
-//        public static Operation GivePoints(string accountID, int points)
-//        {
-//            return new Operation("ft3.dev_give_points", new object[] { accountID, points });
-//        }
-//    }
-//}
+#include <memory>
+#include "../../src/GTV/abstract_value_facotry.h"
+#include "../Core/operation.h"
+#include "account.h"
+
+namespace chromia {
+namespace postchain {
+namespace ft3 {
+class AccountDevOperations
+{
+public:
+	static std::shared_ptr<ft3::Operation> Register(std::shared_ptr<AuthDescriptor> auth_descriptor)
+    {
+		std::shared_ptr<ArrayValue> args = auth_descriptor->ToGTV();
+        return std::make_shared<ft3::Operation>("ft3.dev_register_account", args);
+    }
+
+    static std::shared_ptr<ft3::Operation> FreeOp(std::string account_id)
+    {
+		std::shared_ptr<ArrayValue> args;
+		args->Add(AbstractValueFactory::Build(account_id));
+		return std::make_shared<ft3::Operation>("ft3.dev_free_op", args);
+    }
+
+    static std::shared_ptr<ft3::Operation> GivePoints(string account_id, int points)
+    {
+		std::shared_ptr<ArrayValue> args;
+		args->Add(AbstractValueFactory::Build(account_id));
+		args->Add(AbstractValueFactory::Build(points));
+		return std::make_shared<ft3::Operation>("ft3.dev_give_points", args);
+    }
+};
+} // namespace ft3
+} // namespace postchain
+} // namespace chromia

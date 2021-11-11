ee0f44c537dd42792ca6898b9b5c6aef9852da16
yevhendev645 <yevhorob@gmail.com>
2021-11-11 19:16:39 +0200

    BlockchainSession

---
+++ FT3/Core/Blockchain/blockchain_session.cpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "blockchain_session.h"
+#include "../../User/user.h"
+#include "../../User/account.h"
+#include "./blockchain.h"
+#include "../operation.h"
+
+namespace chromia {
+namespace postchain {
+namespace ft3 {
+
+BlockchainSession::BlockchainSession(std::shared_ptr<User> user, std::shared_ptr<Blockchain> blockchain)
+{
+	this->user_ = user;
+	this->blockchain_ = blockchain;
+}
+
+void BlockchainSession::GetAccountById(string id, std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error)
+{
+	Account::GetById(id, std::shared_ptr<BlockchainSession>(this), on_success, on_error);
+}
+
+void BlockchainSession::GetAccountsByParticipantId(std::string id, std::function<void(std::vector<std::shared_ptr<Account>>)> on_success, std::function<void(std::string)> on_error)
+{
+	Account::GetByParticipantId(id, std::shared_ptr<BlockchainSession>(this), on_success, on_error);
+}
+
+void BlockchainSession::GetAccountsByAuthDescriptorId(string id, std::function<void(std::vector<std::shared_ptr<Account>>)> on_success, std::function<void(std::string)> on_error)
+{
+	Account::GetByAuthDescriptorId(id, std::shared_ptr<BlockchainSession>(this), on_success, on_error);
+}
+
+void BlockchainSession::Query(string query_name, std::vector<QueryObject> query_objects,
+	std::function<void(std::string)> on_success, std::function<void(string)> on_error)
+{
+	this->blockchain_->Query(query_name, query_objects, on_success, on_error);
+}
+
+void BlockchainSession::Call(std::shared_ptr<Operation> operation,
+	std::function<void()> on_success, std::function<void(string)> on_error)
+{
+	this->blockchain_->Call(operation, this->user_, on_success, on_error);
+}
+
+} // namespace ft3
+} // namespace postchain
+} // namespace chromia
--- FT3/Core/Blockchain/blockchain_session.h
+++ FT3/Core/Blockchain/blockchain_session.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "../../User/user.h"
-#include "../../User/account.h"
-#include "./blockchain.h"
+#include "../../forward_declarations.h"
 
 namespace chromia {
 namespace postchain {
@@ -14,39 +12,19 @@ public:
 	std::shared_ptr<User> user_;
     std::shared_ptr<Blockchain> blockchain_;
 
-    BlockchainSession(std::shared_ptr<User> user, std::shared_ptr<Blockchain> blockchain)
-    {
-        this->user_ = user;
-        this->blockchain_ = blockchain;
-    }
+	BlockchainSession(std::shared_ptr<User> user, std::shared_ptr<Blockchain> blockchain);
 
-    void GetAccountById(string id, std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error)
-    {
-        //Account.GetById(id, this, onSuccess, onError);
-    }
+	void GetAccountById(string id, std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error);
 
-   /* void GetAccountsByParticipantId(std::string id, Action<Account[]> onSuccess, Action<string> onError)
-    {
-        yield return Account.GetByParticipantId(id, this, onSuccess, onError);
-    }
+	void GetAccountsByParticipantId(std::string id, std::function<void(std::vector<std::shared_ptr<Account>>)> on_success, std::function<void(std::string)> on_error);
 
-    public IEnumerator GetAccountsByAuthDescriptorId(string id, Action<Account[]> onSuccess, Action<string> onError)
-    {
-        yield return Account.GetByAuthDescriptorId(id, this, onSuccess, onError);
-    }
-	*/
+	void GetAccountsByAuthDescriptorId(string id, std::function<void(std::vector<std::shared_ptr<Account>>)> on_success, std::function<void(std::string)> on_error);
 
-    void Query(string query_name, std::vector<QueryObject> query_objects, 
-		std::function<void(std::string)> on_success, std::function<void(string)> on_error)
-    {
-        this->blockchain_->Query(query_name, query_objects, on_success, on_error);
-    }
+	void Query(string query_name, std::vector<QueryObject> query_objects,
+		std::function<void(std::string)> on_success, std::function<void(string)> on_error);
 	
-    void Call(ft3::Operation operation, 
-		std::function<void()> on_success, std::function<void(string)> on_error)
-    {
-        //this->blockchain_->Call(operation, this->user_, on_success, on_error);
-    }
+	void Call(std::shared_ptr<Operation> operation,
+		std::function<void()> on_success, std::function<void(string)> on_error);
 };
 
 } // namespace ft3

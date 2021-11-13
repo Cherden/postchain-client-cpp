49447bdd8d25d23c0dbc0aed41c8da9baad58600
yevhendev645 <yevhorob@gmail.com>
2021-11-13 17:18:15 +0200

    AccountBuilder

--- tests/TestUtil/account_builder.cpp
+++ tests/TestUtil/account_builder.cpp
@@ -4,12 +4,18 @@
 
 #include "CoreMinimal.h" // TO-DO get rid of UE4 dependencies
 #include "../../../ChromaUnreal/Utils.h"
+#include "../../src/common.h"
 #include "../../FT3/User/account.h"
 #include "../../FT3/User/user.h"
 #include "../../FT3/Core/key_pair.h"
-#include "../../src/common.h"
+#include "../../FT3/User/rate_limit.h"
+#include "../../FT3/User/asset.h"
+#include "../../FT3/User/account.h"
+#include "../../FT3/User/asset_balance.h"
+#include "../../FT3/Core/Blockchain/blockchain.h"
 
-AccountBuilder::AccountBuilder(std::shared_ptr<Blockchain> blockchain, std::shared_ptr<User> user)
+AccountBuilder::AccountBuilder(std::shared_ptr<Blockchain> blockchain, std::shared_ptr<User> user) :
+	flags_({ FlagsType::eAccount, FlagsType::eTransfer })
 {
 	std::shared_ptr <User> new_user = user;
 	if (user == nullptr)
@@ -22,7 +28,7 @@ AccountBuilder::AccountBuilder(std::shared_ptr<Blockchain> blockchain, std::shar
 	this->user_ = new_user;
 }
 
-std::shared_ptr<AccountBuilder> AccountBuilder::CreateAccountBuilder(std::shared_ptr<Blockchain> blockchain, std::shared_ptr<User> user = nullptr)
+std::shared_ptr<AccountBuilder> AccountBuilder::CreateAccountBuilder(std::shared_ptr<Blockchain> blockchain, std::shared_ptr<User> user)
 {
 	return std::make_shared<AccountBuilder>(blockchain, user);
 }
@@ -67,6 +73,91 @@ std::shared_ptr<AccountBuilder> AccountBuilder::WithRequiredSignatures(int count
 	return std::shared_ptr<AccountBuilder>(this);
 }
 
+void AccountBuilder::Build(std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error)
+{
+	if (on_error == nullptr) on_error = this->DefaultErrorHandler;
+
+	std::shared_ptr<Account> account = nullptr;
+
+	this->RegisterAccount([&account](std::shared_ptr<Account> _account) {
+		account = _account;
+	}, on_error);
+
+	if (account != nullptr)
+	{
+		this->AddBalanceIfNeeded(account, []() {}, on_error);
+		this->AddPointsIfNeeded(account, [&account](std::shared_ptr<RateLimit> rate_limit) {
+			account->rate_limit_ = rate_limit;
+		}, on_error);
+
+		on_success(account);
+	}
+}
+
+void AccountBuilder::RegisterAccount(std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error)
+{
+	Account::Register(
+		this->GetAuthDescriptor(),
+		this->blockchain_->NewSession(this->user_),
+		on_success, on_error
+	);
+}
+
+void AccountBuilder::AddBalanceIfNeeded(std::shared_ptr<Account> account, std::function<void()> on_success, std::function<void(string)> on_error)
+{
+	if (this->asset_ != nullptr && this->balance_ != -1)
+	{
+		return AssetBalance::GiveBalance(account->id_, this->asset_->id_,
+			this->balance_, this->blockchain_, on_success, on_error);
+	}
+}
+
+void AccountBuilder::AddPointsIfNeeded(std::shared_ptr<Account> account, std::function<void(std::shared_ptr<RateLimit>)> on_success, std::function<void(std::string)> on_error)
+{
+	if (this->points_ > 0)
+	{
+		return RateLimit::GivePoints(account->id_, this->points_, this->blockchain_, []() {}, on_error);
+	}
+
+	RateLimit::GetByAccountRateLimit(account->id_, this->blockchain_, on_success, on_error);
+}
+
+std::shared_ptr<AuthDescriptor> AccountBuilder::GetAuthDescriptor()
+{
+	if (this->required_signatures_count_ > this->participants_.size())
+	{
+		throw std::exception("Number of required signatures has to be less than number of participants");
+	}
+
+	std::vector<std::vector<byte>> participants;
+
+	if (this->participants_.size() > 1)
+	{
+		for (auto participant : this->participants_)
+		{
+			participants.push_back(participant->pub_key_);
+		}
+
+		std::shared_ptr<MultiSignatureAuthDescriptor> multi = std::make_shared<MultiSignatureAuthDescriptor>(
+			participants,
+			this->required_signatures_count_,
+			this->flags_,
+			this->user_->auth_descriptor_->rule_
+			);
+
+		return std::dynamic_pointer_cast<AuthDescriptor>(multi);
+	}
+	else
+	{
+		std::shared_ptr<SingleSignatureAuthDescriptor> multi = std::make_shared<SingleSignatureAuthDescriptor>(
+			this->participants_[0]->pub_key_,
+			this->flags_,
+			this->user_->auth_descriptor_->rule_
+			);
+
+		return std::dynamic_pointer_cast<AuthDescriptor>(multi);
+	}
+}
 
 void AccountBuilder::DefaultErrorHandler(std::string error)
 {
--- tests/TestUtil/account_builder.h
+++ tests/TestUtil/account_builder.h
@@ -6,11 +6,7 @@
 #include <vector>
 #include "test_user.h"
 #include "../../FT3/forward_declarations.h"
-#include "../../FT3/User/rate_limit.h"
-#include "../../FT3/User/asset.h"
-#include "../../FT3/User/account.h"
-#include "../../FT3/User/asset_balance.h"
-#include "../../FT3/Core/Blockchain/blockchain.h"
+#include "../../FT3/forward_declarations.h"
 
 using namespace chromia::postchain::ft3;
 
@@ -31,26 +27,7 @@ public:
 
 	std::shared_ptr<AccountBuilder> WithRequiredSignatures(int count);
 
-	void build(std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error = nullptr)
-	{
-		if (on_error == nullptr) on_error = this->DefaultErrorHandler;
-
-		std::shared_ptr<Account> account = nullptr;
-
-		this->RegisterAccount([&account](std::shared_ptr<Account> _account) {
-			account = _account;
-		}, on_error);
-
-		if (account != nullptr)
-		{
-			this->AddBalanceIfNeeded(account, [] () { }, on_error);
-			this->AddPointsIfNeeded(account, [&account] (std::shared_ptr<RateLimit> rate_limit) {
-				account->rate_limit_ = rate_limit;
-			}, on_error);
-
-			on_success(account);
-		}
-	}
+	void Build(std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error = nullptr);
 
 private:
 	std::shared_ptr<Blockchain> blockchain_;
@@ -59,76 +36,18 @@ private:
 	std::shared_ptr<Asset> asset_;
 	std::vector<std::shared_ptr<KeyPair>> participants_ = { std::make_shared<KeyPair>() };
 	int required_signatures_count_ = 1;
-	std::vector<FlagsType> flags_ = { FlagsType::eAccount, FlagsType::eTransfer };
+	std::vector<FlagsType> flags_;
 	int points_ = 0;
 
-	void DefaultErrorHandler(std::string error);
+	static void DefaultErrorHandler(std::string error);
 
-	void RegisterAccount(std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error)
-	{
-		Account::Register(
-			this->GetAuthDescriptor(),
-			this->blockchain_->NewSession(this->user_),
-			on_success, on_error
-		);
-	}
+	void RegisterAccount(std::function<void(std::shared_ptr<Account>)> on_success, std::function<void(std::string)> on_error);
 
-	void AddBalanceIfNeeded(std::shared_ptr<Account> account, std::function<void()> on_success, std::function<void(string)> on_error)
-	{
-		if (this->asset_ != nullptr && this->balance_ != -1)
-		{
-			return AssetBalance::GiveBalance(account->id_, this->asset_->id_,
-				this->balance_, this->blockchain_, on_success, on_error);
-		}
-	}
+	void AddBalanceIfNeeded(std::shared_ptr<Account> account, std::function<void()> on_success, std::function<void(string)> on_error);
 
-	void AddPointsIfNeeded(std::shared_ptr<Account> account, std::function<void(std::shared_ptr<RateLimit>)> on_success, std::function<void(std::string)> on_error)
-	{
-		if (this->points_ > 0)
-		{
-			return RateLimit::GivePoints(account->id_, this->points_, this->blockchain_, [] () { }, on_error);
-		}
+	void AddPointsIfNeeded(std::shared_ptr<Account> account, std::function<void(std::shared_ptr<RateLimit>)> on_success, std::function<void(std::string)> on_error);
 
-		RateLimit::GetByAccountRateLimit(account->id_, this->blockchain_, on_success, on_error);
-	}
-
-	std::shared_ptr<AuthDescriptor> GetAuthDescriptor()
-	{
-		if (this->required_signatures_count_ > this->participants_.size())
-		{
-			throw std::exception("Number of required signatures has to be less than number of participants");
-		}
-
-		std::vector<std::vector<byte>> participants;
-
-		if (this->participants_.size() > 1)
-		{
-			for(auto participant : this->participants_)
-			{
-				participants.push_back(participant->pub_key_);
-			}
-
-			std::shared_ptr<MultiSignatureAuthDescriptor> multi = std::make_shared<MultiSignatureAuthDescriptor>(
-				participants,
-				this->required_signatures_count_,
-				this->flags_,
-				this->user_->auth_descriptor_->rule_
-			);
-
-			return std::dynamic_pointer_cast<AuthDescriptor>(multi);
-		}
-		else
-		{
-			std::shared_ptr<SingleSignatureAuthDescriptor> multi = std::make_shared<SingleSignatureAuthDescriptor>(
-				participants,
-				this->participants_[0]->pub_key_,
-				this->flags_,
-				this->user_->auth_descriptor_->rule_
-				);
-
-			return std::dynamic_pointer_cast<AuthDescriptor>(multi);
-		}
-	}
+	std::shared_ptr<AuthDescriptor> GetAuthDescriptor();
 
 };
 

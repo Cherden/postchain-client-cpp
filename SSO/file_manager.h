0300d3e483da1a0df740ad6c15d1603a7a78193d
yevhendev645 <yevhorob@gmail.com>
2021-11-27 14:50:31 +0200

    SSO/FileManager

---
+++ SSO/file_manager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <memory>
+
+namespace chromia {
+namespace postchain {
+namespace ft3 {
+
+class FileManager
+{
+public:
+
+	static bool WriteToFile(std::string a_file_name, std::string a_file_contents);
+
+	static bool LoadFromFile(std::string a_file_name, std::string &result);
+};
+
+} // namespace ft3
+} // namespace postchain
+} // namespace chromia

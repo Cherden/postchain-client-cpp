218b6fd4dce241a2385345de1cd85fdfa2b6b7de
evhendev645 <yevhorob@gmail.com>
2023-02-09 14:39:08 +0200

    UE5 vs UE4 BYTE fix

--- src/PostchainClient/common.h
+++ src/PostchainClient/common.h
@@ -6,3 +6,24 @@
 namespace chromia {
 	typedef unsigned char byte;
 }  // namespace chromia
+
+// If compilation is outside UE, or in UE version 4, then define BYTE as const char. 
+// In UE version 5, there is already BYTE structure available
+
+#ifdef CHROMIA_INSIDE_UNREAL_ENGINE
+	#include "Runtime/Launch/Resources/Version.h"
+	#if ENGINE_MAJOR_VERSION == 4
+		namespace chromia {
+			typedef unsigned char BYTE;
+		}  // namespace chromia
+	#endif
+#else
+	namespace chromia {
+		typedef unsigned char BYTE;
+	}  // namespace chromia
+#endif
+
+//#ifndef BYTE
+//#define BYTE byte
+//#endif
+
